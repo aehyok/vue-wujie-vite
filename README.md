25f5294448b94692d532b844c3dc0ac688690a75
刘启明 <455043818@qq.com>
2024-03-01 11:46:59 +0800

    feat: add vite+ts 系统路径b别名

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # vue-wujie-vite
-vue3 wujie vite5
+根目录下执行命令
+```
+pnpm i
+
+// 一起启动apps下的三个项目
+pnpm start
+
+// 如果想启动apps下的单个项目
+pnpm start --filter dvs-main
+```
--- apps/dvs-ncdp/vite.config.ts
+++ apps/dvs-ncdp/vite.config.ts
@@ -1,11 +1,35 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from 'node:path'
+
+// 当使用文件系统路径的别名时，请始终使用绝对路径。相对路径的别名值会原封不动地被使用，因此无法被正常解析。
+// path.resolve () 方法用于将一系列路径段解析为绝对路径。它通过处理从右到左的路径序列来工作，在每个路径之前添加，直到创建绝对路径。
+function pathResolve(dir: string) {
+  return resolve(process.cwd(), '.', dir)
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  // 别名
+  resolve: {
+    alias: [
+      // @/xxxx => src/xxxx
+      {
+        find: /\@\//,
+        replacement: `${pathResolve('src')}/`,
+      },
+      // #/xxxx => types/xxxx
+      {
+        find: /\#\//,
+        replacement: `${pathResolve('types')}/`,
+      },
+    ],
+    dedupe: ['vue'],
+  },
+
   server: {
-    port: 120,
+    port: 6200,
     host: '0.0.0.0', // 可查看到局域网IP地址
     cors: true, // 默认启用并允许任何源
     // open: true, // 在服务器启动时自动在浏览器中打开应用程序
